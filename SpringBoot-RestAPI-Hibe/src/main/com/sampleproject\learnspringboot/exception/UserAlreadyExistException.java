a0118af8a2129f8a20c027a90432a93c49c6c408
rbgaikwad <81613211+rbgaikwad@users.noreply.github.com>
2022-08-01 11:30:31 +0530

    Add files via upload

---
+++ SpringBoot-RestAPI-Hibe/src/main/com/sampleproject\learnspringboot/exception/UserAlreadyExistException.java
@@ -0,0 +1,14 @@
+package com.sampleproject.learnspringboot.exception;
+
+public class UserAlreadyExistException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public UserAlreadyExistException() {
+		super();
+	}
+	
+	public UserAlreadyExistException(String message) {
+		super(message);
+	}
+}
