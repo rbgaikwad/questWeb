961434261846ee2020799e3dddb84dd3f37f6880
rbgaikwad <81613211+rbgaikwad@users.noreply.github.com>
2022-08-01 23:04:34 +0530

    Update AddressRepository.java

--- SpringBoot-RestAPI-Hibe/src/main/com/sampleproject\learnspringboot/repository/AddressRepository.java
+++ SpringBoot-RestAPI-Hibe/src/main/com/sampleproject\learnspringboot/repository/AddressRepository.java
@@ -1,10 +1,10 @@
 package com.sampleproject.learnspringboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sampleproject.learnspringboot.model.Address;
 
-public interface AddressRepository extends CrudRepository<Address, Integer>{
-
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long>{
 }
